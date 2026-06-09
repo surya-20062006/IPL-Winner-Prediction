51fbf9920227f035eaa85e8ed8021ee039d8a0a2
Surya <sa2049505@gmail.com>
2026-06-09 10:31:23 +0530

    Added categorical feature encoding

--- src/train_model.py
+++ src/train_model.py
@@ -1,29 +1,20 @@
 import os
 import pandas as pd
 
+from sklearn.model_selection import train_test_split
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+
 print("Current Directory:")
 print(os.getcwd())
 
-from sklearn.model_selection import train_test_split
-
-# Load dataset
 final_df = pd.read_csv("data/final_df.csv")
 
-print(final_df.head())
-print(final_df.shape)
-
-# Features
 X = final_df.drop('result', axis=1)
-
-# Target
 y = final_df['result']
 
 print("\nX Shape :", X.shape)
 print("y Shape :", y.shape)
-
-from sklearn.model_selection import train_test_split
-
-# Split Dataset
 
 X_train, X_test, y_train, y_test = train_test_split(
     X,
@@ -31,6 +22,26 @@ X_train, X_test, y_train, y_test = train_test_split(
     test_size=0.2,
     random_state=42
 )
+
+trf = ColumnTransformer(
+    [
+        (
+            'trf',
+            OneHotEncoder(
+                sparse_output=False,
+                handle_unknown='ignore'
+            ),
+            ['batting_team', 'bowling_team']
+        )
+    ],
+    remainder='passthrough'
+)
+
+trf.fit(X_train)
+
+encoded_data = trf.transform(X_train)
+
+print(encoded_data.shape)
 
 print("\nTraining Data")
 print(X_train.shape)
