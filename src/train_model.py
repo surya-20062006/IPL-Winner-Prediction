ad07bd7fb693c11d685baecdcc149edb08114df9
Surya <sa2049505@gmail.com>
2026-06-09 10:00:16 +0530

    Prepared training and testing datasets

--- src/train_model.py
+++ src/train_model.py
@@ -1,2 +1,39 @@
-X = matches_df.drop('result', axis=1)
-y = matches_df['result']
+import os
+import pandas as pd
+
+print("Current Directory:")
+print(os.getcwd())
+
+from sklearn.model_selection import train_test_split
+
+# Load dataset
+final_df = pd.read_csv("data/final_df.csv")
+
+print(final_df.head())
+print(final_df.shape)
+
+# Features
+X = final_df.drop('result', axis=1)
+
+# Target
+y = final_df['result']
+
+print("\nX Shape :", X.shape)
+print("y Shape :", y.shape)
+
+from sklearn.model_selection import train_test_split
+
+# Split Dataset
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X,
+    y,
+    test_size=0.2,
+    random_state=42
+)
+
+print("\nTraining Data")
+print(X_train.shape)
+
+print("\nTesting Data")
+print(X_test.shape)
